d1104ac0db46835d4d05482de3fa69cb68634d1a
Yash Lalluwadia <yash.1331@yahoo.in>
2025-12-05 00:40:59 +0000

    Update scene.js

--- ToonCity/scene.js
+++ ToonCity/scene.js
@@ -33,7 +33,7 @@ export async function buildScene(scene) {
   // Load low-poly city
   const loader = new GLTFLoader();
   return new Promise((resolve, reject) => {
-    loader.load('./City.glb', (gltf) => {
+    loader.load('./city.glb', (gltf) => {
       city = gltf.scene;
       city.scale.set(1, 1, 1);
       city.position.set(0, 0, 0); // You’ll fine-tune this with the gizmo
